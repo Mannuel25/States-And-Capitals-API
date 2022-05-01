87c064c748a16b6866778c98ccbd79be0c826702
mannuel25 <oluwasegunprosperity@gmail.com>
2022-05-01 23:28:19 +0100

    update

--- api/views.py
+++ api/views.py
@@ -3,10 +3,18 @@ from .serializers import StateSerializer, UserSerializer
 from .models import States
 from .permissions import IsAuthorOrReadOnly
 from django.contrib.auth import get_user_model
+from datetime import datetime
 
 class StatesViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
-    queryset = States.objects.all()
+    all_states = States.objects.all()
+    format_today_date = datetime.today().strftime('%Y-%m-%d')
+    queryset = []
+    for i in all_states:
+        # print(i, i.date_created, type(i.date_created))
+        if str(i.date_created) == format_today_date:
+            queryset.append(i)
+    print(queryset)
     serializer_class = StateSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
