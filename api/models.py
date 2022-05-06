5d2f875ff6240784fa280ff8e2986532a180b571
mannuel25 <oluwasegunprosperity@gmail.com>
2022-05-06 23:41:30 +0100

    ..

--- api/models.py
+++ api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
-
 class States(models.Model):
     contributor = models.ForeignKey(User, on_delete=models.CASCADE)
     state = models.CharField(max_length=250)
