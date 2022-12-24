5702f749a5767b88944b2c6c32a8ab6767c11770
mannuel25 <oluwasegunprosperity@gmail.com>
2022-12-24 23:51:47 +0100

    udpate..

--- api/filters.py
+++
@@ -1,9 +0,0 @@
-from django_filters import rest_framework as filters
-from .models import States
-
-class StatesFilter(filters.FilterSet):
-    state = filters.CharFilter(lookup_expr='iexact')
-    
-    class Meta:
-        model = States
-        fields = ['state', 'capital', 'governor', 'slogan','foundation_year','no_of_lgas']
