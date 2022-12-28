94e045009dd1a282c99c87ca8b822d69a77d5caa
mannuel25 <oluwasegunprosperity@gmail.com>
2022-12-28 15:40:05 +0100

    update

--- project/settings.py
+++ project/settings.py
@@ -154,7 +154,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
